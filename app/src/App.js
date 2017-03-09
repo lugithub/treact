e5354d2bd557be383e8bb37e4d08559e86bc181d
bin lu <lu.bin.lu.bin@gmail.com>
2017-03-09 11:58:25 -0500

    choose the react element type at runtime

--- app/src/App.js
+++ app/src/App.js
@@ -1,41 +1,23 @@
+//https://facebook.github.io/react/docs/jsx-in-depth.html#choosing-the-type-at-runtime
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      todos: [
-        {id: 1, name: 'learn', isComplete: true},
-        {id: 2, name: 'build', isComplete: false},
-        {id: 3, name: 'ship', isComplete: false}
-      ]
-    }
-  }
+const PhotoStory = () => <div>ppp</div>;
+const VideoStory = () => <div>vvv</div>;
 
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <div className="todo-app">
-          <form>
-            <input type="text"/>
-          </form>
-          <ul className="todo-list">
-            {this.state.todos.map(todo =>
-              <li key={todo.id}>
-                <input type="checkbox" defaultChecked={todo.isComplete}/>{todo.name}
-              </li>)}
-          </ul>
-        </div>
+//this doesn't work
+//react Component has to return a JSX expression
+//const Story = ({storyType}) => storyType === 'p' ? PhotoStory : VideoStory;
 
-      </div>
-    );
-  }
-}
+//ok
+//better than the way below
+//const Story = ({storyType}) => storyType === 'p' ? <PhotoStory /> : <VideoStory />;
 
-export default App;
+const Story = ({storyType}) => {
+  const X = storyType === 'p' ? PhotoStory : VideoStory;
+  return <X />;
+} ;
+
+export default Story;
--- app/src/index.js
+++ app/src/index.js
@@ -4,6 +4,6 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <App storyType={'v'} />,
   document.getElementById('root')
 );
