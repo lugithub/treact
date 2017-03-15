3c781ab05d71839109006c7d6fe4e74c7929057d
bin lu <lu.bin.lu.bin@gmail.com>
2017-03-15 17:05:59 -0400

    Route: url parameter, optional url parameter

--- app/src/App.js
+++ app/src/App.js
@@ -1,29 +1,22 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Route,
-  NavLink
+  Route
  } from 'react-router-dom';
 import './App.css';
 
-const Links = () => (
-  <nav>
-    <NavLink activeClassName="active" to="/">home</NavLink>
-    <NavLink activeStyle={{color: 'green'}} to={{pathname: '/about'}}>about</NavLink>
-    <NavLink activeClassName="active" isActive={(match, location) => {
-      console.log(match, location);
-      return match;
-    }} to="/contact">contact</NavLink>
-  </nav>
-);
+
 
 const App = () => (
   <Router>
     <div>
-      <Links />
-      <Route exact path="/" render={() => <h1>home</h1>} />
-      <Route path="/about" render={() => <h1>about</h1>} />
-      <Route path="/contact" render={() => <h1>contact</h1>} />
+      <Route path="/:page?-:subpage?" render={({match}) =>
+        <h1>
+          PAGE: {match.params.page || 'home'}<br />
+          SUBPAGE: {match.params.subpage}
+        </h1>
+      }
+      />
     </div>
   </Router>
 );
